af61512e96533667dcfb1b8884af03bba26a0f72
daplerio <dario95tineo@gmail.com>
2016-02-18 11:35:53 +0100

    Arreglo de etiqueta Autowired

--- src/main/java/aplication/web/MainController.java
+++ src/main/java/aplication/web/MainController.java
@@ -1,5 +1,6 @@
 package aplication.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +13,7 @@ import aplication.service.VoterAcces;
 @RestController
 public class MainController {
 
+	@Autowired
 	private VoterAcces voterAccess;
 
 	@RequestMapping("/")
