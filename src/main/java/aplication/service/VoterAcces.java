273cd83b2c7dea2d25671e02943ec9c729deac86
daplerio <dario95tineo@gmail.com>
2016-02-18 11:43:43 +0100

    La aplicación devuelve un 404 NOT FOUND cuando no se encuentra

--- src/main/java/aplication/service/VoterAcces.java
+++ src/main/java/aplication/service/VoterAcces.java
@@ -1,6 +1,7 @@
 package aplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Component;
 
 import aplication.domain.User;
@@ -22,7 +23,12 @@ public class VoterAcces implements GetVoter {
 	public User getUser(String email, String password) {
 		// TODO comprobar email y contraseña not null
 		this.repository.save(new User("pamela@gmail.com", "patata"));
-		return this.repository.findByEmailAndPassword(email, password);
+		User user = this.repository.findByEmailAndPassword(email, password);
+		if (user == null) {
+			throw new ResourceNotFoundException("El usuario no se encuentra en la base de datos");
+		} else {
+			return user;
+		}
 	}
 
 }
