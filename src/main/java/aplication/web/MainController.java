4690e290c1c00b76e07013607e46b826d631ed49
daplerio <dario95tineo@gmail.com>
2016-02-24 10:11:23 +0100

    Ahora se puede cambiar la contraseña

--- src/main/java/aplication/web/MainController.java
+++ src/main/java/aplication/web/MainController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import aplication.UserInfo;
 import aplication.domain.Gizmo;
 import aplication.domain.ServerResponse;
+import aplication.domain.User;
+import aplication.service.DBManagement;
 import aplication.service.VoterAcces;
 import groovy.lang.Grab;
 
@@ -19,6 +21,12 @@ import groovy.lang.Grab;
 
 @Controller
 public class MainController {
+
+	@Autowired
+	private DBManagement repository;
+	
+	private UserInfo userInfo;
+	private ServerResponse serverResponse;
 
 	@Autowired
 	private VoterAcces voterAccess;
@@ -32,22 +40,50 @@ public class MainController {
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
 	@ResponseBody
 	public Object getVI(@RequestBody UserInfo userInfo) {
-		return this.voterAccess.getVoter(userInfo.getEmail(), userInfo.getPassword());
+
+		this.userInfo = userInfo;
+
+		serverResponse = this.voterAccess.getVoter(userInfo.getEmail(), userInfo.getPassword());
+
+		return serverResponse;
 	}
 
 	@RequestMapping(value = "/showUserInfo", method = RequestMethod.POST)
 	public String showUserInfo(Gizmo gizmo, Model model) {
 
 		ServerResponse response;
-		
+
 		try {
 			response = this.voterAccess.getVoter(gizmo.getField1(), gizmo.getField2());
+			this.serverResponse = response;
+			userInfo = new UserInfo(gizmo.getField1(), gizmo.getField2());
 		} catch (Exception e) {
 			return "WelcomePage";
 		}
 
 		ArrayList<Object> atributos = new ArrayList<>();
 		atributos.add(response);
+
+		model.addAttribute("atributes", atributos);
+
+		return "InfoPage";
+	}
+
+	@RequestMapping(value = "/changePassword", method = RequestMethod.POST)
+	public String changePassword(Model model) {
+		model.addAttribute(new Gizmo());
+		return "ChangePassword";
+	}
+
+	@RequestMapping(value = "/changingPassword", method = RequestMethod.POST)
+	public String changingPassword(Gizmo gizmo, Model model) {
+		
+		User user = repository.findByEmailAndPassword(userInfo.getEmail(), userInfo.getPassword());
+		user.setPassword(gizmo.getField1());
+		repository.save(user);
+		
+		ArrayList<Object> atributos = new ArrayList<>();
+		atributos.add(serverResponse);
 
 		model.addAttribute("atributes", atributos);
 
